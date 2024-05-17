8e51c4f1c2fe202eea556816bcfd6cb69ea646c0
hsanch13 <hayleysanchez13@gmail.com>
2024-05-16 20:19:26 -0600

    created components and imported them into App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+import WelcomePage from "./WelcomePage"
+import NameCollection from "./NameCollection"
+import NewNameForm from "./NewNameForm"
+
+function App() {
+  return (
+    <div className="App">
+      hello 
+    </div>
+  );
+}
+
+export default App;
