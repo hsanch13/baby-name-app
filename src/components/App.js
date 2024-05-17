9fdb04b594b0bff3fe4e6053649db070cf3837f3
hsanch13 <hayleysanchez13@gmail.com>
2024-05-16 21:21:39 -0600

    updated css styles and added semantic ui welcome page component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import WelcomePage from "./WelcomePage"
 
 function App() {
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,13 +2,15 @@ import React, {useState} from 'react'
 import NameCollection from "./NameCollection"
 import NewNameForm from "./NewNameForm"
 import Search from "./Search"
+import { Container } from "semantic-ui-react";
 
 function WelcomePage() {
+    
     const [babyNames, setBabyNames] = useState([])
 
   return (
     <div>
-      
+      hello
     </div>
   )
 }
