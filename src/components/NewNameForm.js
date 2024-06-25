4de41a08512c6bd4088c20009c872f1a866b19c1
hsanch13 <hayleysanchez13@gmail.com>
2024-06-25 10:49:43 -0400

    added a form to NewNameForm component

--- src/components/NewNameForm.js
+++ src/components/NewNameForm.js
@@ -6,7 +6,41 @@ function NewNameForm() {
     <div>
       <h3>Add a baby name</h3>
       <Form>
-
+      <Form.Group widths="equal">
+          <Form.Input 
+            fluid 
+            label="Name" 
+            placeholder="Name" 
+            name="name" 
+            // value={}
+            // onChange={}
+            />
+          <Form.Input
+            fluid 
+            label="hp" 
+            placeholder="hp" 
+            name="hp" 
+            // value={} 
+            // onChange={}
+            />
+          <Form.Input
+            fluid
+            label="Front Image URL"
+            placeholder="url"
+            name="frontUrl"
+            // value={}
+            // onChange={} 
+          />
+          <Form.Input
+            fluid
+            label="Back Image URL"
+            placeholder="url"
+            name="backUrl"
+            // value={} 
+            // onChange={}
+          />
+        </Form.Group>
+        <Form.Button>Submit</Form.Button>
       </Form>
     </div>
   )
