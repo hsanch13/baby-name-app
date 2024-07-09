411c210e3ad96250f544e0338fa001072895d86f
hsanch13 <hayleysanchez13@gmail.com>
2024-07-09 14:59:37 -0600

    added links for routes on WelcomePage

--- src/components/App.js
+++ src/components/App.js
@@ -34,6 +34,11 @@ function App() {
   return (
     <Router>
       <div className="App">
+      <nav>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/addNewName" className="nav-link">Add New Name</Link>
+          <Link to="/search" className="nav-link">Search</Link>
+        </nav>
         <Routes>
           <Route
             path="/"
